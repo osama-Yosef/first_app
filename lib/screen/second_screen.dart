18e6d081a7b623b1a5e70965d9ad0f7391bb52e7
osama yosef <osamayosef038@gmail.com>
2025-06-27 14:32:48 +0300

    my Bmi App

--- lib/screen/second_screen.dart
+++ lib/screen/second_screen.dart
@@ -13,8 +13,8 @@ class SecondScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double heightByM=hight/100;
-    double bmiResult=weight/(heightByM*heightByM);
+    double heightByM = hight / 100;
+    double bmiResult = weight / (heightByM * heightByM);
 
     return Scaffold(
       backgroundColor: MyColors.mainColor,
@@ -91,25 +91,25 @@ class SecondScreen extends StatelessWidget {
   }
 }
 
-
-statusText(double bmiResult){
-  if(bmiResult<18.5){
+statusText(double bmiResult) {
+  if (bmiResult < 18.5) {
     return "underweight";
-  }else if(bmiResult<24.9){
+  } else if (bmiResult < 24.9) {
     return "Normal";
-  }else if(bmiResult<29.9){
+  } else if (bmiResult < 29.9) {
     return "overweight";
-  }else {
+  } else {
     return "obesity";
   }
 }
-description(double bmiResult){
-  if(bmiResult<18.5){
+description(double bmiResult) {
+  if (bmiResult < 18.5) {
     return "Your body weight is lower than normal. This might be due to nutritional deficiency or other health conditions. It's recommended to consult a healthcare provider to gain weight healthily.";
-  }else if(bmiResult<24.9){
+  } else if (bmiResult < 24.9) {
     return "You have a healthy body weight. Keep maintaining your current lifestyle with balanced nutrition and regular physical activity.";
-  }else if(bmiResult<29.9){
+  } else if (bmiResult < 29.9) {
     return "You are slightly above the recommended weight for your height. Consider adopting a more active lifestyle and healthier eating habits to reduce health risks.";
-  }else {
+  } else {
     return "You are at a higher risk of health problems related to excess body weight, such as heart disease, diabetes, and high blood pressure. It's important to consult a healthcare provider to develop a weight loss plan.";
-  }}
+  }
+}
